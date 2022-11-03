4fb6555c8eb52f749642cceb5668db376b057f4b
tania <taniaosano15@gmail.com>
2022-11-03 21:42:45 +0300

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
-  
+     # a review belongs to a game
+
+    belongs_to :game
+
+    # def game
+    #   Game.find(self.game_id)
+    # end
 end
+
+# in the commented code above, the custom instance method is equivalent to belongs_to macro method
+# self is the review instance. game is in singular
